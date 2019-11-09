0161b5fce94d3f702377749cbfc5b34a934a01b4
lucas <lsminteris@gmail.com>
2019-11-09 13:42:37 -0800

    messed around with looks in index.css, added links, picture, and input field

--- stream-list/src/App.js
+++ stream-list/src/App.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <br></br>
+        <img src="https://ewscripps.brightspotcdn.com/dims4/default/393121b/2147483647/strip/true/crop/1024x576+0+54/resize/1280x720!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2Fca%2F1c%2F0b4f926b4409979cfdd2ff743dca%2Fgettyimages-1054424572.jpg" alt="Italian Trulli" />
         <p>
-          Add your favorite streamers here!
+          There are only two sites you can add streams currently, mixer and twitch. We are working on being able to integrate more websites. 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitch.tv"
           target="_blank"
           rel="noopener noreferrer"
+          class="label"
         >
-          Learn React
+          twitch
         </a>
+        <a
+          className="App-link"
+          href="https://mixer.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="label"
+        >
+          mixer
+        </a>
+        <br></br>
+        <p>
+          Add your favorite streamers here!
+        </p>
         <form>
           <label>
             Streamer's link:
@@ -25,6 +39,7 @@ function App() {
           </label>
           <input type="submit" value="Submit" />
         </form>
+        <br></br>
       </header>
     </div>
   );
